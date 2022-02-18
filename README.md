6da8cdf0a0006bba27c08ab08fb0594c4cd0e7ef
Lucas Hen <oisouhen@gmail.com>
2022-02-18 17:54:03 -0300

    Phone

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Phone
- 
+-------
+
+#### Creative
+> Connections: ghmattimysql, ghmattimysqljs.
+
+-------
+
+#### Crensa
+Você pode entrar em nosso grupo e ter acesso a atualizações e diversos canais informantes sobre a Base - [Discord](https://discord.gg/9hevnqj6th).
+
+-------
---
+++ database/database.sql
@@ -0,0 +1,41 @@
+USE `summerz`;
+
+DROP TABLE IF EXISTS `phone_calls`;
+CREATE TABLE `phone_calls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` varchar(10) NOT NULL,
+  `num` varchar(10) NOT NULL,
+  `incoming` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `accepts` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `phone_chat`;
+CREATE TABLE `phone_chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channel` varchar(10) NOT NULL,
+  `message` varchar(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `phone_contacts`;
+CREATE TABLE `phone_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(60) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `number` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `display` varchar(64) CHARACTER SET utf8mb4 NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `phone_messages`;
+CREATE TABLE `phone_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `transmitter` varchar(10) NOT NULL,
+  `receiver` varchar(10) NOT NULL,
+  `message` varchar(255) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isRead` int(11) NOT NULL DEFAULT 0,
+  `owner` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
